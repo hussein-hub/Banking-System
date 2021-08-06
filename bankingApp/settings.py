7f6bb064dae5fc631ed1e683a43d99b8885c613d
hussein-hub <hussein.m@somaiya.edu>
2021-08-06 13:05:02 +0530

    updated settings

--- bankingApp/settings.py
+++ bankingApp/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cgk2_f2kw@vqd5n21wafc(cq52_51ns(s2gz)42uc#sp2*-uhv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['banking-system-hussein.herokuapp.com']
 
 
 # Application definition
