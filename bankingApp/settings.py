c02558f8013bf6a9ff224b0a3207d020b2cb4530
hussein-hub <hussein.m@somaiya.edu>
2021-08-07 11:45:48 +0530

    Fixed sidebar issues

--- bankingApp/settings.py
+++ bankingApp/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SECRET_KEY = 'cdca95b2e9b9940b45238072b3127dea8ba770af0d6768ce'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
